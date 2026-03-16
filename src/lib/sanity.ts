81dd32e80bdc868bbabca37bfe66d909866ac9ea
Rysonic Media <rysonicmedia@gmail.com>
2026-03-16 12:53:27 +0530

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client';
 export const client = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID || 'n2kl5ru9',
   dataset: 'production',
-  useCdn: true,
+  useCdn: false,
   apiVersion: '2024-01-01',
   token: import.meta.env.VITE_SANITY_TOKEN,
 });
